340d16686aa3c96efe02fd980e5e92963e7a9c4f
MaxM <lynchnost@gmail.com>
2017-06-18 23:17:13 +0300

    Rewrote intro, using classes

--- js/templates/intro.js
+++ js/templates/intro.js
@@ -1,28 +1,41 @@
 import {views, initialState} from '../data/gamedata';
-import getElementFromTemplate from '../utils/get-element-from-template';
-import insertTemplate from '../utils/insert-template';
-import renderGreeting from './greeting';
-
-
+import IntroView from './intro-view';
+// import getElementFromTemplate from '../utils/get-element-from-template';
+// import insertTemplate from '../utils/insert-template';
+// import renderGreeting from './greeting';
+//
+//
+// export default (state) => {
+//   const template = getElementFromTemplate(`
+//   <div id="main" class="central__content">
+//     <div id="intro" class="intro">
+//       <h1 class="intro__asterisk" title="Начать игру">*</h1>
+//       <p class="intro__motto"><sup>*</sup> Это не&nbsp;фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+//     </div>
+//   </div>`);
+//
+//
+//   const startBtn = template.querySelector(`.intro__asterisk`);
+//
+//   startBtn.addEventListener(`click`, () => {
+//     state = Object.assign({}, initialState, {
+//       template: views.greeting
+//     });
+//
+//     insertTemplate(renderGreeting(state));
+//   });
+//
+//   return template;
+// };
 export default (state) => {
-  const template = getElementFromTemplate(`
-  <div id="main" class="central__content">
-    <div id="intro" class="intro">
-      <h1 class="intro__asterisk" title="Начать игру">*</h1>
-      <p class="intro__motto"><sup>*</sup> Это не&nbsp;фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
-    </div>
-  </div>`);
+  const intro = new IntroView();
 
-
-  const startBtn = template.querySelector(`.intro__asterisk`);
-
-  startBtn.addEventListener(`click`, () => {
+  intro.onClick = () => {
     state = Object.assign({}, initialState, {
       template: views.greeting
     });
+    console.log(state);
+  };
 
-    insertTemplate(renderGreeting(state));
-  });
-
-  return template;
+  return intro.element;
 };
