5e284ddc739667da31f3464955b649908d70941b
MaxM <lynchnost@gmail.com>
2017-06-27 00:12:40 +0300

    Hz routing

--- js/main.js
+++ js/main.js
@@ -3,28 +3,78 @@ import GreetingScreen from './greeting/greeting';
 import RulesScreen from './rules/rules';
 import GameScreen from './game/game';
 import StatsScreen from './stats/stats';
+import {initialState} from './data/gamedata';
+
+
+const ControllerID = {
+  INTRO: ``,
+  GREETING: `greeting`,
+  RULES: `rules`,
+  GAME: `game`,
+  STATS: `stats`,
+};
+
+const getControllerIDFromHash = (hash) => hash.replace(`#`, ``);
 
 
 export default class Application {
+  constructor() {
+    this.routes = {
+      [ControllerID.INTRO]: IntroScreen,
+      [ControllerID.GREETING]: GreetingScreen,
+      [ControllerID.RULES]: RulesScreen,
+      [ControllerID.GAME]: GameScreen,
+      [ControllerID.STATS]: StatsScreen,
+    };
+
+    this.currentState = initialState;
+
+    window.onhashchange = () => {
+      this.changeController(getControllerIDFromHash(location.hash));
+    };
+  }
+
+  changeController(route = ``) {
+    const Controller = this.routes[route];
+
+    new Controller(this.currentState).init();
+  }
+
+  init() {
+    Application.showIntro();
+    this.changeController(getControllerIDFromHash(location.hash));
+  }
+
   static showIntro() {
-    new IntroScreen().init();
+    // new IntroScreen().init();
+    location.hash = ControllerID.INTRO;
   }
 
   static showGreeting(state) {
-    new GreetingScreen(state).init();
+    // new GreetingScreen(state).init();
+    this.currentState = state;
+    location.hash = ControllerID.INTRO;
   }
 
   static showRules(state) {
-    new RulesScreen(state).init();
+    // new RulesScreen(state).init();
+    this.currentState = state;
+    location.hash = ControllerID.RULES;
   }
 
   static showGame(state) {
-    new GameScreen(state).init();
+    // new GameScreen(state).init();
+    this.currentState = state;
+    location.hash = ControllerID.GAME;
   }
 
   static showStats(stats) {
-    new StatsScreen(stats).init();
+    // new StatsScreen(stats).init();
+    this.currentState = stats;
+    location.hash = ControllerID.STATS;
   }
 }
 
-Application.showIntro();
+new Application().init();
+
+// Application.showIntro();
