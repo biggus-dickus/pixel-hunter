940698c44c8f0e7b685ff40b480d2bcbb53015d2
Max Maslenko <maxm@netvertise.com>
2017-07-03 19:20:20 +0300

    Stats fix WIP

--- js/stats/stats-view.js
+++ js/stats/stats-view.js
@@ -9,69 +9,134 @@ export default class StatsView extends AbstractView {
   constructor(state) {
     super();
     this._state = state;
+    this.caption = (this._state.lives > 0) ? `Победа!` : `POTRACHENO!`;
   }
 
-  insertPlayerName() {
+  insertCaption(caption) {
     if (this._state.playerName && this._state.serverStats) {
-      return `<p>Статистика по&nbsp;пользователю: ${this._state.playerName}</p>`;
+      return `Лучшие игры ${this._state.playerName}`;
     }
 
-    return ``;
+    return `${caption}`;
+  }
+
+  // get template() {
+  //   // We show the last successful game for certain player only if statistics was directly accessed via URL (e. g. origin/#stats=Vasya).
+  //   // In all other cases we render stats based on local data.
+  //   const stats = (this._state.serverStats) ? this._state.serverStats[this._state.serverStats.length - 1].stats : recordedAnswers;
+  //
+  //   const fastAnswersCount = stats.filter((answer) => answer === FAST_ANSWER_FLAG).length;
+  //   const regularSpeedAnswersCount = stats.filter((answer) => answer === CORRECT_ANSWER_FLAG).length;
+  //   const slowAnswersCount = stats.filter((answer) => answer === SLOW_ANSWER_FLAG).length;
+  //
+  //   this._state.correctAnswers = regularSpeedAnswersCount + fastAnswersCount + slowAnswersCount;
+  //   this._state.slowAnswers = slowAnswersCount;
+  //   this._state.fastAnswers = fastAnswersCount;
+  //   this._state.lives = (this._state.serverStats) ? this._state.serverStats[this._state.serverStats.length - 1].lives : this._state.lives;
+  //
+  //
+  //   const results = getResults(this._state);
+  //
+  //   return `<header class="header"></header>
+  //     <div class="result">
+  //       <h1>${this.insertCaption(results.caption)}</h1>
+  //       <table class="result__table">
+  //         <tr>
+  //           <td class="result__number">1.</td>
+  //           <td colspan="2" id="stats-td"></td>
+  //           <td class="result__points">×&nbsp;${rates.correctAnswerPoints}</td>
+  //           <td class="result__total">${results.correctPoints}</td>
+  //         </tr>
+  //         <tr>
+  //           <td></td>
+  //           <td class="result__extra">Бонус за&nbsp;скорость:</td>
+  //           <td class="result__extra">${this._state.fastAnswers}&nbsp;<span class="stats__result stats__result--fast"></span></td>
+  //           <td class="result__points">×&nbsp;${rates.fastAnswerPoints}</td>
+  //           <td class="result__total">${results.fastPoints}</td>
+  //         </tr>
+  //         <tr>
+  //           <td></td>
+  //           <td class="result__extra">Бонус за&nbsp;жизни:</td>
+  //           <td class="result__extra">${this._state.lives}&nbsp;<span class="stats__result stats__result--heart"></span></td>
+  //           <td class="result__points">×&nbsp;${rates.lifeBonusPoints}</td>
+  //           <td class="result__total">${results.lifeBonusPoints}</td>
+  //         </tr>
+  //         <tr>
+  //           <td></td>
+  //           <td class="result__extra">Штраф за&nbsp;медлительность:</td>
+  //           <td class="result__extra">${this._state.slowAnswers}&nbsp;<span class="stats__result stats__result--slow"></span></td>
+  //           <td class="result__points">×&nbsp;${rates.slowAnswerPoints}</td>
+  //           <td class="result__total">${results.slowPokePoints}</td>
+  //         </tr>
+  //         <tr>
+  //           <td colspan="5" class="result__total  result__total--final">${results.total}</td>
+  //         </tr>
+  //       </table>
+  //     </div>`;
+  // }
+
+  renderStatsTable() {
+    const tableMarkup = [];
+
+    const stats = (this._state.serverStats) ? this._state.serverStats : recordedAnswers;
+
+    for (let [i, item] of stats.entries()) {
+      const fastAnswersCount = (this._state.serverStats) ? item.stats.filter((answer) => answer === FAST_ANSWER_FLAG).length : item.filter((answer) => answer === FAST_ANSWER_FLAG).length;
+
+      const regularSpeedAnswersCount = (this._state.serverStats) ? item.stats.filter((answer) => answer === CORRECT_ANSWER_FLAG).length : item.filter((answer) => answer === CORRECT_ANSWER_FLAG).length;
+
+      const slowAnswersCount = (this._state.serverStats) ? item.stats.filter((answer) => answer === SLOW_ANSWER_FLAG).length : item.filter((answer) => answer === SLOW_ANSWER_FLAG).length;
+
+      this._state.correctAnswers = regularSpeedAnswersCount + fastAnswersCount + slowAnswersCount;
+      this._state.slowAnswers = slowAnswersCount;
+      this._state.fastAnswers = fastAnswersCount;
+      this._state.lives = (this._state.serverStats) ? item.lives : this._state.lives;
+
+
+      const results = getResults(this._state);
+
+      tableMarkup.push(`<table class="result__table">
+            <tr>
+              <td class="result__number">${++i}.</td>
+              <td colspan="2" id="stats-td"></td>
+              <td class="result__points">×&nbsp;${rates.correctAnswerPoints}</td>
+              <td class="result__total">${results.correctPoints}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td class="result__extra">Бонус за&nbsp;скорость:</td>
+              <td class="result__extra">${this._state.fastAnswers}&nbsp;<span class="stats__result stats__result--fast"></span></td>
+              <td class="result__points">×&nbsp;${rates.fastAnswerPoints}</td>
+              <td class="result__total">${results.fastPoints}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td class="result__extra">Бонус за&nbsp;жизни:</td>
+              <td class="result__extra">${this._state.lives}&nbsp;<span class="stats__result stats__result--heart"></span></td>
+              <td class="result__points">×&nbsp;${rates.lifeBonusPoints}</td>
+              <td class="result__total">${results.lifeBonusPoints}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td class="result__extra">Штраф за&nbsp;медлительность:</td>
+              <td class="result__extra">${this._state.slowAnswers}&nbsp;<span class="stats__result stats__result--slow"></span></td>
+              <td class="result__points">×&nbsp;${rates.slowAnswerPoints}</td>
+              <td class="result__total">${results.slowPokePoints}</td>
+            </tr>
+            <tr>
+              <td colspan="5" class="result__total  result__total--final">${results.total}</td>
+            </tr>
+          </table>`);
+    }
+
+    return tableMarkup.join(``);
   }
 
   get template() {
-    // We show the last successful game for certain player only if statistics was directly accessed via URL (e. g. origin/#stats=Vasya).
-    // In all other cases we render stats based on local data.
-    const stats = (this._state.serverStats) ? this._state.serverStats[this._state.serverStats.length - 1].stats : recordedAnswers;
-
-    const fastAnswersCount = stats.filter((answer) => answer === FAST_ANSWER_FLAG).length;
-    const regularSpeedAnswersCount = stats.filter((answer) => answer === CORRECT_ANSWER_FLAG).length;
-    const slowAnswersCount = stats.filter((answer) => answer === SLOW_ANSWER_FLAG).length;
-
-    this._state.correctAnswers = regularSpeedAnswersCount + fastAnswersCount + slowAnswersCount;
-    this._state.slowAnswers = slowAnswersCount;
-    this._state.fastAnswers = fastAnswersCount;
-    this._state.lives = (this._state.serverStats) ? this._state.serverStats[this._state.serverStats.length - 1].lives : this._state.lives;
-
-
-    const results = getResults(this._state);
-
     return `<header class="header"></header>
       <div class="result">
-        <h1>${results.caption}</h1>
-        ${this.insertPlayerName()}
-        <table class="result__table">
-          <tr>
-            <td class="result__number">1.</td>
-            <td colspan="2" id="stats-td"></td>
-            <td class="result__points">×&nbsp;${rates.correctAnswerPoints}</td>
-            <td class="result__total">${results.correctPoints}</td>
-          </tr>
-          <tr>
-            <td></td>
-            <td class="result__extra">Бонус за&nbsp;скорость:</td>
-            <td class="result__extra">${this._state.fastAnswers}&nbsp;<span class="stats__result stats__result--fast"></span></td>
-            <td class="result__points">×&nbsp;${rates.fastAnswerPoints}</td>
-            <td class="result__total">${results.fastPoints}</td>
-          </tr>
-          <tr>
-            <td></td>
-            <td class="result__extra">Бонус за&nbsp;жизни:</td>
-            <td class="result__extra">${this._state.lives}&nbsp;<span class="stats__result stats__result--heart"></span></td>
-            <td class="result__points">×&nbsp;${rates.lifeBonusPoints}</td>
-            <td class="result__total">${results.lifeBonusPoints}</td>
-          </tr>
-          <tr>
-            <td></td>
-            <td class="result__extra">Штраф за&nbsp;медлительность:</td>
-            <td class="result__extra">${this._state.slowAnswers}&nbsp;<span class="stats__result stats__result--slow"></span></td>
-            <td class="result__points">×&nbsp;${rates.slowAnswerPoints}</td>
-            <td class="result__total">${results.slowPokePoints}</td>
-          </tr>
-          <tr>
-            <td colspan="5" class="result__total  result__total--final">${results.total}</td>
-          </tr>
-        </table>
+        <h1>${this.insertCaption(this.caption)}</h1>
+        ${this.renderStatsTable()}
       </div>`;
   }
 
